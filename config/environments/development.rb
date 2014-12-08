9d2f257e4f33a217735299c4e54fdf401bd31051
mojo1643 <johnrejimathai@gmail.com>
2014-12-08 10:30:56 +0530

    Internal server error corrected

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  
+  #config.threadsafe = true
+  config.middleware.delete Rack::Lock
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
