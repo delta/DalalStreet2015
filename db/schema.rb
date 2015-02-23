1c4ed96a1ed8f58b1b407a9a6b0d97bfa8992a2c
Rishikc <msrishi88@gmail.com>
2015-02-23 19:29:03 +0530

    somepics

--- db/schema.rb
+++ db/schema.rb
@@ -31,14 +31,6 @@ ActiveRecord::Schema.define(version: 20150223112939) do
     t.datetime "updated_at"
   end
 
-  create_table "leaderboards", force: true do |t|
-    t.string   "username"
-    t.integer  "user_id"
-    t.decimal  "total",      precision: 4, scale: 2
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "logs", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
